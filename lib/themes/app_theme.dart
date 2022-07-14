278731549cf7eb48006b2d77d6214e129e891578
Jose Perez <font3face@gmail.com>
2022-07-14 15:39:00 -0500

    add cards to card widget

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.red;
+  static const Color primary = Colors.indigo;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       primaryColor: primary,
       appBarTheme: const AppBarTheme(color: primary, elevation: 0),
---
+++ lib/widgets/custom_card_type_2.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/themes/app_theme.dart';
+
+class CustomCardType2 extends StatelessWidget {
+  const CustomCardType2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+      elevation: 10,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
+      child: Column(
+        children: [
+          const FadeInImage(
+            image: NetworkImage(
+                'https://frases.top/wp-content/uploads/2020/01/imagen-de-fornite.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Videojuego del año'),
+          )
+        ],
+      ),
+    );
+  }
+}
