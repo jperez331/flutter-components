ac61cd5e09097d91ad84e421e127d2f0822fce34
Jose Perez <font3face@gmail.com>
2022-07-18 14:37:52 -0500

    add style and alert to alertScreen

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cerrar'))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              displayDialog(context);
+            },
             child: const Padding(
               padding: EdgeInsets.all(20),
               child: Text(
